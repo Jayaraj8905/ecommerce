821d610d2364b8324a3f789165d42feb679517c2
Jayaraj <jayaraj8905@gmail.com>
2019-08-17 20:13:45 +0800

    User Schema

--- server/src/user/user.model.js
+++ server/src/user/user.model.js
@@ -2,11 +2,37 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    username: { type: String, unique: true, required: true },
+    username: { 
+        type: String, 
+        unique: true, 
+        required: [true, `Username is required`],
+        minlength: [4, 'Username must have 4 characters'],
+    },
     hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    email: { 
+        type: String, 
+        unique: true, 
+        required: [true, 'Email is required'], 
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }, 
+    },
+    name: { 
+        type: String, 
+        required: [true, 'Name is required'],
+        minlength: [4, 'Name must have 4 characters'],
+    },
+    // role: {},
+    status: { 
+        type: String,
+        enum: ['ACTIVE', 'INACTIVE', 'BLOCKED'], 
+        default: 'ACTIVE' 
+    },
+    createddate: { type: Date, default: Date.now },
+    udpateddate: { type: Date, default: Date.now }
 });
 
 schema.set('toJSON', { virtuals: true });
